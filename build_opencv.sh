68cc02b7f43e51ca19b1d379066b4819f76b2ea6
Christopher Bunn <bunn@yeti.local>
2014-10-21 20:52:39 -0500

    Updated to use newer version, opencv-2.4.10

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,7 +1,8 @@
-curl -L 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.7/opencv-2.4.7.tar.gz/' | tar xvzf -
-mkdir -p opencv-2.4.7/release
-cd opencv-2.4.7/release
+curl -LO 'http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.10/opencv-2.4.10.zip'
+unzip opencv-2.4.10.zip
+mkdir -p opencv-2.4.10/release
+cd opencv-2.4.10/release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON -D WITH_XINE=ON -D WITH_TBB=ON ..
 make && make install
 cd /
-rm -rf opencv-2.4.7
+rm -rf opencv-2.4.10
